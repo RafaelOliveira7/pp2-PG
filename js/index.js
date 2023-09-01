273d9447ee3fcde44a7a8bd1d02bf4dd1ffb0446
Carlos Daniel Silva <carlosdaniel.sp@iclou.com>
2023-09-01 00:12:46 -0300

    finalização do projeto

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+document.addEventListener("click", () => {
+    torusMaterial.wireframe = !torusMaterial.wireframe;
+    d20material.wireframe = !d20material.wireframe;
+    esferaMaterial.wireframe = !esferaMaterial.wireframe;
+});
+
+var d20pulse = 1;
+
 // GL
 const vShader = `
 varying vec2 v_uv;
@@ -47,10 +55,16 @@ const scene = new THREE.Scene();
 // set clock
 const clock = new THREE.Clock();
 
+
+const camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
 // camera
 let aspect = window.innerWidth / window.innerHeight;
-const camera = new THREE.PerspectiveCamera( 15, aspect, 1, 1000 );
-camera.position.z = 10;
+const topCamera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+topCamera.position.set(0, 10, 0);
+topCamera.lookAt(0, 0, 0);
+
+let activeCamera = camera;
 
 // renderer
 const renderer = new THREE.WebGLRenderer({
@@ -68,49 +82,92 @@ const material = new THREE.ShaderMaterial({
   uniforms
 });
 
+document.addEventListener("keydown", (event) => {
+    if (event.key === "1") {
+        activeCamera = camera;
+    } else if (event.key === "2") {
+        activeCamera = topCamera;
+    }
+});
+
 // mesh
 const cube = new THREE.Mesh(geometry, material);
-scene.add( cube ); // add to scene
+cube.position.set(4,-1,0);
 
-// render
-function render() {
-  // rotate the cube
-  cube.rotation.y += 0.01;
-  cube.rotation.x += 0.01;
-  // update time uniform
-  uniforms.u_time.value = clock.getElapsedTime();
-  // animation loop
-  requestAnimationFrame( render );
-  renderer.render( scene, camera );
+const d20geometry = new THREE.IcosahedronGeometry(1,0);
+const d20material = new THREE.MeshBasicMaterial( { color: 0xf20303 } );
+d20material.wireframe = true;
+const d20 = new THREE.Mesh( d20geometry, d20material );
+d20.scale.set(0.9,0.9,0.9);
+d20.position.set(0,-2,0);
+
+const torusGeomentry = new THREE.TorusGeometry( 1, 0.4, 12, 25 ); 
+const torusMaterial = new THREE.MeshBasicMaterial( { color: 0xdb03fc } ); 
+torusMaterial.wireframe = true;
+const torus = new THREE.Mesh( torusGeomentry, torusMaterial );
+torus.scale.set(0.9,0.9,0.9);
+torus.position.set(0,2,0);
+
+const geoEsfera = new THREE.SphereGeometry(1, 32, 32);
+const esferaMaterial = new THREE.MeshBasicMaterial({color: 0x00ABFF });
+esferaMaterial.wireframe = true;
+const esfera = new THREE.Mesh(geoEsfera, esferaMaterial);
+esfera.scale.set(0.9,0.9,0.9);
+
+camera.position.z = 5;
+camera.lookAt(0, 0, 0);
+
+scene.add( torus, d20, esfera, cube );
+
+const initialPosition = new THREE.Vector3(0, 0, -5);
+const bounceDirection = new THREE.Vector3(1, 1, 1).normalize();
+const bounceSpeed = 0.05;
+
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    topCamera.aspect = window.innerWidth / window.innerHeight;
+    topCamera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
 }
 
-// mousemove
-document.addEventListener('mousemove', (e) =>{
-   uniforms.u_mouse.value.x = e.clientX;
-   uniforms.u_mouse.value.y = e.clientY;
- })
-// window resize
-function onWindowResize( event ) {
-  const aspectRatio = window.innerWidth/window.innerHeight;
-  let width, height;
-  if (aspectRatio>=1){
-    width = 1;
-    height = (window.innerHeight/window.innerWidth) * width;
-  }else{
-    width = aspectRatio;
-    height = 1;
-  }
-  camera.left = -width;
-  camera.right = width;
-  camera.top = height;
-  camera.bottom = -height;
-  camera.updateProjectionMatrix();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  if (uniforms.u_resolution !== undefined){
-    uniforms.u_resolution.value.x = window.innerWidth;
-    uniforms.u_resolution.value.y = window.innerHeight;
-  }
+window.addEventListener( 'resize', onWindowResize );
+
+
+function animate() {
+    requestAnimationFrame( animate );
+
+    cube.rotation.y += 0.01;
+    cube.rotation.x += 0.01;
+    uniforms.u_time.value = clock.getElapsedTime();
+
+    torus.rotation.x += 0.02;
+
+    d20.rotation.y += 0.02;
+    d20.scale.set(d20pulse%3, d20pulse%3, d20pulse%3);
+    d20pulse++;
+
+    esfera.rotation.x += 0.01;
+    esfera.rotation.y += 0.02;
+
+    esfera.position.add(bounceDirection.clone().multiplyScalar(bounceSpeed));
+
+    if (esfera.position.x > 10 || esfera.position.x < -10) {
+        bounceDirection.x *= -1;
+    }
+    if (esfera.position.y > 5 || esfera.position.y < -5) {
+        bounceDirection.y *= -1;
+    }
+    if (esfera.position.z > -1 || esfera.position.z < -10) {
+        bounceDirection.z *= -1;
+    }
+
+    renderer.render( scene, activeCamera );
 }
 
-// run everything
-render();
+animate();
